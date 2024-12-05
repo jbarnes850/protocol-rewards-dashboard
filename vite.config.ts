288789214a574cd4e006de637fbca15d8068612c
Elliot Braem <elliot@ejlbraem.com>
2024-12-05 17:10:14 -0600

    adds vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [react(), vercel()],
   define: {
     'process.env': {},
   },
